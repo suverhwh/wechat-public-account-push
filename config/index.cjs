b886e467a1d250be4a0391db7a1650dac7a4b427
suverhwh <39234761+suverhwh@users.noreply.github.com>
2023-09-20 18:28:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,18 +33,18 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1998', date: '08-11',
+          type: '生日', name: '宝贝', year: '1998', date: '08-11',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2020', date: '07-26',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2021-01-23' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2024-01-23' },
       ],
     },
   ],
